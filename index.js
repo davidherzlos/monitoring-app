fbd32e15f304a06f1e025389826540b23303aee3
David OC <davidherzlos@gmail.com>
2023-08-12 17:50:52 -0600

    feat: add service for users

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const http = require('http')
 const https = require('https')
 const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
-const config = require('./config')
+const config = require('./lib/config')
 const fs = require('fs')
+const handlers = require ('./lib/handlers')
+const helpers = require('./lib/helpers')
 
 // Instantiate the HTTP server.
 const httpServer = http.createServer((req, res) => {
@@ -83,7 +85,7 @@ const unifiedServer = (req, res) => {
             'queryStringObject': queryStringObject,
             'method': method,
             'headers': headers,
-            'payload': buffer
+            'payload': helpers.parseJsonToObject(buffer),
         }
 
         // Route the request to the handler specified in the router.
@@ -109,21 +111,8 @@ const unifiedServer = (req, res) => {
     })
 }
 
-// Define the handlers.
-const handlers = {}
-
-// Ping handler.
-handlers.ping = (data, callback) => {
-    // Callback a http status code and a payload optionally.
-    callback(200)
-}
-
-// Not found handler.
-handlers.notFound = (data, callback) => {
-    callback(404)
-}
-
 // Define a request router.
 const router = {
-    'ping': handlers.ping
+    'ping': handlers.ping,
+    'users': handlers.users
 }
